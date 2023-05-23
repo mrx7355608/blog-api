b521716b3d23df7d0ad600655206ae32dede9c43
mrx7355608 <mrx7355608@gmail.com>
2023-05-23 17:41:47 +0500

    passportjs setup

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ const config = {
     port: process.env.PORT,
     dbUrl: process.env.DB_URL,
     clientUrl: process.env.CLIENT_URL,
-    nodeEnv: process.env.NODE_ENV
+    nodeEnv: process.env.NODE_ENV,
+    sessionSecret: process.env.SESSION_SECRET
 }
 
 export default config
---
+++ src/passportSetup.js
@@ -0,0 +1,26 @@
+import passport from 'passport'
+import { Strategy } from 'passport-local'
+
+passport.use(
+    new Strategy({ usernameField: 'email' }, function (email, password, done) {
+        // Check if user exists
+        const user = await userDB.findByEmail(email)
+        if (!user) return done(null, false)
+
+        // Check if passowrds match
+        const isValidPassword = await hashServices.compare(password, user.password)
+        if (!isValidPassword) {
+            return done(null, true, { message: 'Incorrect email or password' })
+        }
+
+        return done(null, user)
+    })
+)
+
+passport.serializeUser(function (user, done) {
+    return done(null, user.id)
+})
+passport.deserializeUser(function (id, done) {
+    const user = await usersDB.findById(id)
+    return done(null, user)
+})
