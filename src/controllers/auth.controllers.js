482718171e038cf48fc51d6de5e4454ec593fe1a
mrx7355608 <mrx7355608@gmail.com>
2023-05-28 21:29:59 -0700

    added more auth tests

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -8,57 +8,62 @@ const hashingServices = HashingServices()
 const authServices = AuthServices({ usersDB, hashingServices })
 
 export default function AuthControllers() {
-    // SIGNUP 
-    async function signup(httpRequestObject) {
-        const data = httpRequestObject.body
+  // SIGNUP 
+  async function signup(httpRequestObject) {
+    const data = httpRequestObject.body
 
-        if (!data || Object.keys(data).length < 1) {
-            throw new ApiError('No data is given', 400)
-        }
-
-        const userData = {
-            fname: data.fname,
-            lname: data.lname,
-            email: data.email,
-            password: data.password,
-            confirmPassword: data.confirmPassword
-        }
-
-        await authServices.signup(userData)
-
-        return {
-            statusCode: 201,
-            body: { message: 'Thank you for signing up' }
-        }
-    }
-    
-    // LOGIN (passportjs)
-    async function login(req, res, next) {
-        passport.authenticate('local', function (err, user, info) {
-            if (err) return next(err)
-            if (!user) return res.status(404).json({ error: 'User does not exist' })
-            if (info) return res.status(400).json({ error: info.message })
-
-            req.logIn(user, function (err) {
-                if (err) return next(err)
-                return res.status(200).json({ user })
-            })
-        })(req, res, next)
+    if (!data || Object.keys(data).length < 1) {
+      throw new ApiError('No data is given', 400)
     }
 
-    // LOGOUT (passportjs)
-    async function logout(req, res, next) {
-        req.logout(function (err) {
-            if (err) return next(err)
-            return res.status(200).json({
-                logout: 'success'
-            })
-        })
+    const userData = {
+      fname: data.fname,
+      lname: data.lname,
+      email: data.email,
+      password: data.password,
+      confirmPassword: data.confirmPassword
     }
+
+    await authServices.signup(userData)
 
     return {
-        signup,
-        login,
-        logout
+      statusCode: 201,
+      body: { message: 'Thank you for signing up' }
     }
+  }
+
+  // LOGIN (passportjs)
+  async function login(req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) return next(err)
+      if (!user) return res.status(404).json({ error: 'User does not exist' })
+      if (info) return res.status(400).json({ error: info.message })
+
+      req.logIn(user, function (err) {
+        if (err) return next(err)
+        const userData = {
+          fullname: user.fullname,
+          photo: user.photo,
+          id: user.id
+        }
+        return res.status(200).json({ user: userData })
+      })
+    })(req, res, next)
+  }
+
+  // LOGOUT (passportjs)
+  async function logout(req, res, next) {
+    req.logout(function (err) {
+      if (err) return next(err)
+      return res.status(200).json({
+        logout: 'success'
+      })
+    })
+  }
+
+  return {
+    signup,
+    login,
+    logout
+  }
 }
--- tests/auth/e2e.test.js
+++ tests/auth/e2e.test.js
@@ -9,46 +9,90 @@ const agent = request(app)
 const baseUrl = '/api/v1/auth'
 
 describe('E2E Tests', function () {
-    beforeAll(async function() {
-        await DBManager.Connect(config.dbUrl)
+  beforeAll(async function() {
+    await DBManager.Connect(config.dbUrl)
+  })
+  afterAll(async function() {
+    await DBManager.Disconnect()
+  })
+
+  const url = baseUrl + '/signup'
+
+  describe('POST /auth/signup', function () {
+    it('should return error if user is already registered', async function () {
+      const fakeUser = createFakeUser()
+      const response = await agent.post(url)
+        .send({
+          ...fakeUser,
+          email: 'Rosa.Thompson96@hotmail.com',
+          confirmPassword: fakeUser.password 
+        })
+        .expect(400)
+
+      expect(response.body.error).toBe('User already exists')
     })
-    afterAll(async function() {
-        await DBManager.Disconnect()
+
+    it('should return error when no data is provided', async function () {
+      const response = await agent.post(url)
+        .send(null).expect(400)
+
+      expect(response.body.error).toBe('No data is given')
     })
 
-    const url = baseUrl + '/signup'
-
-    describe('Signup', function () {
-        it('should return error if user is already registered', async function () {
-            const fakeUser = createFakeUser()
-            const response = await agent.post(url)
-                .send({
-                    ...fakeUser,
-                    email: 'Rosa.Thompson96@hotmail.com',
-                    confirmPassword: fakeUser.password 
-                })
-                .expect(400)
-
-            expect(response.body.error).toBe('User already exists')
+    it.skip('should create a new user', async function () {
+      const fakeUser = createFakeUser()
+      const response = await agent
+        .post(url)
+        .send({ 
+          ...fakeUser, confirmPassword: fakeUser.password 
         })
 
-        it('should return error when no data is provided', async function () {
-            const response = await agent.post(url)
-                .send(null).expect(400)
-
-            expect(response.body.error).toBe('No data is given')
-        })
-
-        it.skip('should create a new user', async function () {
-            const fakeUser = createFakeUser()
-            const response = await agent
-                .post(url)
-                .send({ 
-                    ...fakeUser, confirmPassword: fakeUser.password 
-                })
-
-            console.log(response.body)
-            expect(response.body.message).toBe('Thank you for signing up')
-        })
+      expect(response.body.message).toBe('Thank you for signing up')
     })
+  })
+
+  describe('POST /auth/login', function () {
+    it('should return error on invalid creds', async function () {
+      const response = await agent.post('/api/v1/auth/login').send({
+        email: 'fawad@gmail.com',
+        password: '12312312312'
+      })
+
+      expect(response.body.error).toBe('Incorrect email or password')
+    }) 
+
+    it('should login user and should not return any sensitive data', async function () {
+      const response = await agent.post('/api/v1/auth/login').send({
+        email: 'fawad@gmail.com',
+        password: 'fawadimran101010'
+      }).expect(200)
+
+      expect(response.body.user).toEqual({
+        id: expect.any(String),
+        fullname: expect.any(String),
+        photo: expect.any(String),
+      })
+    }) 
+
+  })
+
+  describe('POST /auth/logout', function () {
+    it('should return error if user is not logged in, but tries to logout', async function () {
+      const response = await agent.post('/api/v1/auth/logout').expect(401)
+      expect(response.body.error).toBe('You must login first')
+    }) 
+
+    it('should logout user', async function () {
+      // Login user before logging out
+      const response1 = await agent.post('/api/v1/auth/login').send({
+        email: 'fawad@gmail.com',
+        password: 'fawadimran101010'
+      })
+      const cookies = response1.headers['set-cookie']
+      
+      // Logout
+      const response2 = await agent.post('/api/v1/auth/logout').set('Cookie', cookies).expect(200)
+      expect(response2.body.logout).toBe('success')
+    })
+  })
 })
