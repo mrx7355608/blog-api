af9c01fd21f5197947ec4d28827b9fd8dcb22dd1
mrx7355608 <mrx7355608@gmail.com>
2023-05-28 20:39:14 -0700

    modified user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,67 +1,69 @@
 import mongoose from 'mongoose'
 
-const socialSchema = new mongoose.Schema({
-    facebook: {
-        type: String,
-        default: ''
-    },
-    twitter: {
-        type: String,
-        default: ''
-    },
-    instagram: {
-        type: String,
-        default: ''
-    },
-    linkedIn: {
-        type: String,
-        default: ''
-    },
-})
-
 const userSchema = new mongoose.Schema({
-    fname: {
-        type: String,
-        required: true
-    },
-    lname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    photo: {
-        type: String,
-        default: ''
-    },
-    password: {
-        type: String,
-        required: true 
-    },
-    blogs: {
-        type: [mongoose.Schema.Types.ObjectId],
-        default: []
-    },
-    bio: {
-        type: String,
-        default: ''
-    },
-    writingCategories: {
-        type: [String],
-        default: []
-    },
-    socialLinks: socialSchema
+  fname: {
+    type: String,
+    required: true
+  },
+  
+  lname: {
+    type: String,
+    required: true
+  },
+  
+  email: {
+    type: String,
+    required: true
+  },
+  
+  photo: {
+    type: String,
+    default: ''
+  },
+  
+  password: {
+    type: String,
+    required: true 
+  },
+  
+  bio: {
+    type: String,
+    default: ''
+  },
+  
+  writingCategories: {
+    type: [String],
+    default: []
+  },
+  
+  facebook: {
+    type: String,
+    default: ''
+  }, 
+  
+  twitter: {
+    type: String,
+    default: ''
+  }, 
+  
+  instagram: {
+    type: String,
+    default: ''
+  }, 
+  
+  linkedIn: {
+    type: String,
+    default: ''
+  },
 }, {
     timestamps: true,
     toJSON: {
-        virtuals: true
+      virtuals: true
     }
-})
+  })
 
 userSchema.virtual('fullname').get(function() {
-    return `${this.fname} ${this.lname}`
+  return `${this.fname} ${this.lname}`
 })
 
 const UserModel = mongoose.model('User', userSchema)
