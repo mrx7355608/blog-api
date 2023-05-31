d3edd46f23a4c46a081689d30a9fb6b88b0a599d
mrx7355608 <mrx7355608@gmail.com>
2023-05-30 21:09:37 -0700

    indentation and comma changes

--- src/app.js
+++ src/app.js
@@ -1,47 +1,51 @@
-import express from 'express'
-import helmet from 'helmet'
-import hpp from 'hpp'
-import cors from 'cors'
-import morgan from 'morgan'
-import config from '../config/index.js'
-import passport from 'passport'
-import session from 'express-session'
-import { catch404, globalErrorHandler } from './utils/errorHandlers.js'
-import authRouter from './routes/auth.routes.js'
-import userRouter from './routes/user.routes.js'
-import './passportSetup.js'
+import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cors from 'cors';
+import morgan from 'morgan';
+import config from '../config/index.js';
+import passport from 'passport';
+import session from 'express-session';
+import { catch404, globalErrorHandler } from './utils/errorHandlers.js';
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import './passportSetup.js';
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(hpp())
-app.use(cors({
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(hpp());
+app.use(
+  cors({
     origin: config.clientUrl,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 // TODO: setup mongodb store for sessions
-app.use(session({
+app.use(
+  session({
     secret: config.sessionSecret,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true
-    }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/user', userRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/user', userRouter);
 
 // ERROR HANDLERS
-app.use(catch404)
-app.use(globalErrorHandler)
+app.use(catch404);
+app.use(globalErrorHandler);
 
-export default app
+export default app;
